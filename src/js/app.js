5c91b144777490a718c2d2d93ff3a529e3074e09
Darko Bozhinovski <dbozhinovski@gmail.com>
2018-05-09 12:34:57 +0200

    Removes cms config

--- src/js/app.js
+++ src/js/app.js
@@ -14,14 +14,3 @@ ReactDOM.render(<Links />, document.querySelector(".nav-social"));
 if (document.querySelector(".social-share")) {
   ReactDOM.render(<Sharer />, document.querySelector(".social-share"));
 }
-
-// JS Goes here - ES6 supported
-if (window.netlifyIdentity) {
-  window.netlifyIdentity.on("init", (user) => {
-    if (!user) {
-      window.netlifyIdentity.on("login", () => {
-        document.location.href = "/admin/";
-      });
-    }
-  });
-}
